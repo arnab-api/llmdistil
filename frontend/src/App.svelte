f01b6e1d13dff7bdaf2d25f0e6fdbd6355d03bb0
arnab-api <arnab.api@gmail.com>
2024-03-08 20:42:39 -0500

    added sveltekit and tested data transfer between the front and back ends

---
+++ frontend/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let rand = -1;
+    function getRand() {
+        fetch("http://localhost:5000/rand")
+            .then((d) => d.text())
+            .then((d) => (rand = d));
+        console.log("rand: ", rand);
+    }
+</script>
+
+<h1>Your number is {rand}!</h1>
+<button on:click={getRand}>Get a random number</button>
