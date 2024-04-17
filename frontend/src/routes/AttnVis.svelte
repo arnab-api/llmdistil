2c922fff2a611b48fe7fe3ae1a6fbfae69b27901
arnab-api <arnab.api@gmail.com>
2024-04-17 01:17:14 -0400

    added number of views

--- frontend/src/routes/AttnVis.svelte
+++ frontend/src/routes/AttnVis.svelte
@@ -1,25 +1,29 @@
 <script>
-    import * as d3 from 'd3';
-    import {onMount} from 'svelte';
-    import bufferingGif from '../assets/buffering.gif'
+    import * as d3 from "d3";
+    import { onMount } from "svelte";
+    import bufferingGif from "../assets/buffering.gif";
 
     export let attnMatrix = null;
-    export let n_views = 3;
+    export let n_views = "3";
     export let remove_prefix_pad = true;
-    export let selected_heads = [[5, 5], [8, 3], [11, 4]];
+    export let selected_heads = [
+        [5, 5],
+        [8, 3],
+        [11, 4],
+    ];
     export let attn_vis_container;
 
-
-    export async function update_attention_container(){
+    export async function update_attention_container() {
         // container.innerHTML = `<img src="${bufferingGif}" alt="Buffering..." />`;
         if (!attnMatrix || attnMatrix.attention_matrices.length === 0) {
-            console.error('Attention data is required');
+            console.error("Attention data is required");
             return;
         }
+        console.log("n_views = ", n_views, typeof n_views);
         const visualizationElement = visualize_multiple_attention(
             attnMatrix,
             remove_prefix_pad,
-            n_views,
+            parseInt(n_views),
             selected_heads
         );
         attn_vis_container.innerHTML = "";
@@ -39,172 +43,239 @@
     function visualize_multiple_attention(
         attention_mappings,
         remove_prefix_pad = false,
-        n_views = 3, 
+        n_views = 3,
         selected_heads = null
     ) {
-        console.log("selected heads", selected_heads)
-        
-        const colors = [
-            d3.interpolateReds, d3.interpolateBlues, d3.interpolateGreens, 
-            d3.interpolatePurples, d3.interpolateGreys, d3.interpolateOranges
-        ]
+        console.log("selected heads", selected_heads);
 
-        if(n_views > colors.length){
-            throw `n_views more than ${colors.length} is not supported`
+        const colors = [
+            d3.interpolateReds,
+            d3.interpolateBlues,
+            d3.interpolateGreens,
+            d3.interpolatePurples,
+            d3.interpolateGreys,
+            d3.interpolateOranges,
+        ];
+
+        if (n_views > colors.length) {
+            throw `n_views more than ${colors.length} is not supported`;
         }
-        
-        let num_layers = attention_mappings.attention_matrices.length
-        let num_heads = attention_mappings.attention_matrices[0].length
-        let tokens = attention_mappings.tokenized_prompt
+
+        let num_layers = attention_mappings.attention_matrices.length;
+        let num_heads = attention_mappings.attention_matrices[0].length;
+        let tokens = attention_mappings.tokenized_prompt;
         let TARGET_TOKEN = tokens.length - 1;
 
         if (remove_prefix_pad === true) {
             if (tokens[0] === "<|endoftext|>") {
-            tokens = tokens.slice(1, )
-            } 
-            else{
-            remove_prefix_pad = false
+                tokens = tokens.slice(1);
+            } else {
+                remove_prefix_pad = false;
+            }
+        }
+
+        if (selected_heads !== null && selected_heads.length < n_views) {
+            while (selected_heads.length < n_views) {
+                selected_heads.push([0, 0]);
             }
         }
 
         // Function to draw tokens based on layer and head
         function drawTokens(
-            target_idx, 
-            layer_idx, head_idx, 
-            container_div, color
+            target_idx,
+            layer_idx,
+            head_idx,
+            container_div,
+            color
         ) {
-            const attention_matrix = attention_mappings.attention_matrices[layer_idx][head_idx];
-            
-            let attention_distribution = attention_matrix[target_idx]
+            const attention_matrix =
+                attention_mappings.attention_matrices[layer_idx][head_idx];
+
+            let attention_distribution = attention_matrix[target_idx];
             if (remove_prefix_pad === true) {
-                attention_distribution = attention_distribution.slice(1,)
+                attention_distribution = attention_distribution.slice(1);
             }
-        
+
             // Clear previous content
             container_div.html("");
-            let color_scale = d3.scaleSequential()
-                .domain([d3.min(attention_distribution), d3.max(attention_distribution)])
+            let color_scale = d3
+                .scaleSequential()
+                .domain([
+                    d3.min(attention_distribution),
+                    d3.max(attention_distribution),
+                ])
                 .interpolator(color);
-        
+
             tokens.forEach((src_token, i) => {
-                let color_rgb = i <= target_idx ? color_scale(attention_distribution[i]) : "rgb(255, 255, 255)";
+                let color_rgb =
+                    i <= target_idx
+                        ? color_scale(attention_distribution[i])
+                        : "rgb(255, 255, 255)";
                 let text_color = i <= target_idx ? "black" : "gray";
-                    
+
                 let style = `background: ${color_rgb}; color: ${text_color}; padding:2px;`; // Added padding for visibility
                 if (i === target_idx) {
                     style += " border:1px solid black;";
                 }
-                    
-                let token_div = container_div.append('div')
+
+                let token_div = container_div
+                    .append("div")
                     .attr("style", style)
                     .text(src_token);
-            
-                token_div.on('mouseenter', function() {
+
+                token_div.on("mouseenter", function () {
                     if (i !== target_idx) {
                         d3.select(this).style("border", "1px solid gray");
                     }
                 });
-            
-                token_div.on('mouseleave', function() {
+
+                token_div.on("mouseleave", function () {
                     if (i !== target_idx) {
                         d3.select(this).style("border", "");
                     }
                 });
-            
+
                 // Update target_idx and redraw tokens on click
-                token_div.on('click', () => {
+                token_div.on("click", () => {
                     // drawTokens(i, layer_idx, head_idx, container_div, color); // Re-draw tokens with new target_idx
-                    update_all_views(i)
+                    update_all_views(i);
                 });
             });
         }
 
-          // Outer container
-        const main_container_div = d3.create('div')
-            .attr("style", "display: flex; flex-wrap: wrap; flex-direction: column; gap: 1em; padding: 1em;");
+        // Outer container
+        const main_container_div = d3
+            .create("div")
+            .attr(
+                "style",
+                "display: flex; flex-wrap: wrap; flex-direction: column; gap: 1em; padding: 1em;"
+            );
 
-        for(let view_no = 0; view_no < n_views; view_no++){ 
-            const view_container_div = main_container_div.append('div').attr('id', `view_${view_no}`);
-            const color = colors[view_no]
-            let default_head = 0
-            let default_layer = 0
-            if (selected_heads !== null){
-                console.log(">>>>", selected_heads)
-                default_head = selected_heads[view_no][1]
-                default_layer = selected_heads[view_no][0]
+        for (let view_no = 0; view_no < n_views; view_no++) {
+            const view_container_div = main_container_div
+                .append("div")
+                .attr("id", `view_${view_no}`);
+            const color = colors[view_no];
+            let default_head = 0;
+            let default_layer = 0;
+            if (selected_heads !== null) {
+                console.log(">>>>", selected_heads);
+                default_head = selected_heads[view_no][1];
+                default_layer = selected_heads[view_no][0];
             }
-    
+
             // Configuration container for selects
-            const config_div = view_container_div.append('div')
-                .attr("style", "display: flex; flex-wrap: wrap; gap: 0.5em; padding: 0.5em;");
+            const config_div = view_container_div
+                .append("div")
+                .attr(
+                    "style",
+                    "display: flex; flex-wrap: wrap; gap: 0.5em; padding: 0.5em;"
+                );
 
             // Layer Select
-            const layer_config_div = config_div.append('div').attr("style", "display: flex;")
-            layer_config_div.append('label')
-                .attr('for', 'layer-select')
-                .text('Layer: ');
-            
-            const layer_select = layer_config_div.append('select').attr('id', 'layer-select');
-            for (let i = 0; i < num_layers; i++){
-                if(i == default_layer) layer_select.append('option').text(i).attr('value', i).attr("selected", "selected");
-                else layer_select.append('option').text(i).attr('value', i);
-            }    
-    
+            const layer_config_div = config_div
+                .append("div")
+                .attr("style", "display: flex;");
+            layer_config_div
+                .append("label")
+                .attr("for", "layer-select")
+                .text("Layer: ");
+
+            const layer_select = layer_config_div
+                .append("select")
+                .attr("id", "layer-select");
+            for (let i = 0; i < num_layers; i++) {
+                if (i == default_layer)
+                    layer_select
+                        .append("option")
+                        .text(i)
+                        .attr("value", i)
+                        .attr("selected", "selected");
+                else layer_select.append("option").text(i).attr("value", i);
+            }
+
             // Head Select
-            const head_config_div = config_div.append('div').attr("style", "display: flex;")
-            head_config_div.append('label')
-                .attr('for', 'head-select')
-                .text('Head: ');
-        
-            const head_select = head_config_div.append('select').attr('id', 'head-select');
-            for (let i = 0; i < num_heads; i++){
-                if(i == default_head) head_select.append('option').text(i).attr('value', i).attr("selected", "selected");
-                else head_select.append('option').text(i).attr('value', i);
-            }    
+            const head_config_div = config_div
+                .append("div")
+                .attr("style", "display: flex;");
+            head_config_div
+                .append("label")
+                .attr("for", "head-select")
+                .text("Head:");
+
+            const head_select = head_config_div
+                .append("select")
+                .attr("id", "head-select");
+            for (let i = 0; i < num_heads; i++) {
+                if (i == default_head)
+                    head_select
+                        .append("option")
+                        .text(i)
+                        .attr("value", i)
+                        .attr("selected", "selected");
+                else head_select.append("option").text(i).attr("value", i);
+            }
 
             // Visualization container
-            const token_container = view_container_div.append('div')
+            const token_container = view_container_div
+                .append("div")
                 .attr("style", "display: flex; flex-wrap: wrap;")
                 .attr("id", "tokens");
-    
+
             // React to selections
             function updateVisualization() {
                 const selected_layer = +layer_select.node().value;
                 const selected_head = +head_select.node().value;
-                
+
                 let target_idx = TARGET_TOKEN;
-            
-                drawTokens(target_idx, selected_layer, selected_head, token_container, color);
+
+                drawTokens(
+                    target_idx,
+                    selected_layer,
+                    selected_head,
+                    token_container,
+                    color
+                );
             }
-    
+
             // Listen for changes and update
-            layer_select.on('change', updateVisualization);
-            head_select.on('change', updateVisualization);
-    
+            layer_select.on("change", updateVisualization);
+            head_select.on("change", updateVisualization);
+
             // Initial drawing
             updateVisualization();
         }
 
-        function update_all_views(target_idx){
-            TARGET_TOKEN = target_idx
-            for(let view_no = 0; view_no < n_views; view_no++){
-                const layer_selector = d3.select(`#view_${view_no}`).select("#layer-select")
-                const head_selector = d3.select(`#view_${view_no}`).select("#head-select")
-                const token_view = d3.select(`#view_${view_no}`).select("#tokens")
-                
-                console.log(view_no, `L: ${layer_selector.node().value}`, `H: ${head_selector.node().value}`)
+        function update_all_views(target_idx) {
+            TARGET_TOKEN = target_idx;
+            for (let view_no = 0; view_no < n_views; view_no++) {
+                const layer_selector = d3
+                    .select(`#view_${view_no}`)
+                    .select("#layer-select");
+                const head_selector = d3
+                    .select(`#view_${view_no}`)
+                    .select("#head-select");
+                const token_view = d3
+                    .select(`#view_${view_no}`)
+                    .select("#tokens");
+
+                console.log(
+                    view_no,
+                    `L: ${layer_selector.node().value}`,
+                    `H: ${head_selector.node().value}`
+                );
 
                 drawTokens(
-                    target_idx, 
-                    layer_selector.node().value, head_selector.node().value,
-                    token_view, colors[view_no]
-                )
+                    target_idx,
+                    layer_selector.node().value,
+                    head_selector.node().value,
+                    token_view,
+                    colors[view_no]
+                );
             }
         }
         return main_container_div.node();
     }
 </script>
 
-<div bind:this={attn_vis_container}>
-
-</div>
+<div bind:this={attn_vis_container}></div>
--- frontend/src/routes/InputPrompt.svelte
+++ frontend/src/routes/InputPrompt.svelte
@@ -1,15 +1,17 @@
 <script>
     import AttnVis from "./AttnVis.svelte";
     import { onMount } from "svelte";
-    import bufferingGif from '../assets/buffering.gif'
+    import bufferingGif from "../assets/buffering.gif";
 
     export let prompt = "A quick brown fox jumps over the lazy dog";
     export let attnMatrix = null;
     export let attn_vis_container;
+    export let n_views = "3";
 
     export async function getAttnMatrix() {
         try {
             attnMatrix = null;
+            console.log("Fetching attention matrix for prompt:", prompt);
             attn_vis_container.innerHTML = `<img class="buffering-gif" src="${bufferingGif}" alt="Buffering..." />`;
             const response = await fetch(
                 "http://10.200.205.169:5050/attnmatrix?prompt=" +
@@ -46,19 +48,28 @@
         on:input={handleInput}
         placeholder="Enter prompt"
     ></textarea>
-    <div class="button-container">
-        <button class="action-button" on:click={getAttnMatrix}>Go</button>
+    <div class="controls">
+        <div class="button-container">
+            &nbsp;&nbsp;
+            <button class="action-button" on:click={getAttnMatrix}>Go</button>
+        </div>
+        <div class="nview-control">
+            <label for="nview-select">#Heads</label> &nbsp;
+            <select id="nview-select" bind:value={n_views}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3" selected>3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+            </select>
+        </div>
     </div>
 
     <div class="attn-vis-container">
-        <AttnVis {attnMatrix}
-            bind:attn_vis_container 
-        />
+        <AttnVis {attnMatrix} bind:attn_vis_container bind:n_views />
     </div>
-
 </div>
-
-
 
 <style>
     .prompt-and-control {
@@ -67,32 +78,45 @@
         align-items: center;
         justify-content: center;
         padding: 20px;
-        width: 100%; /* Ensures the container takes the full width of the viewport */
-    }
-
-    .input-section {
-        width: 100%; /* Full width of the container */
-        padding: 0 10px; /* Slight padding on the sides */
+        width: 100%; /* Adjust if needed to match the design */
     }
 
     .prompt-input {
-        width: 100%; /* Makes textarea take up all of the container's width */
+        width: 100%; /* Ensures textarea takes up all of the container's width */
         min-height: 100px; /* Minimum height */
-        resize: none; /* Prevent manual resize */
-        padding: 10px; /* Comfortable padding inside the textarea */
+        resize: none; /* Disallow manual resizing */
+        padding: 10px; /* Padding inside the textarea */
         box-sizing: border-box; /* Includes padding in the width */
+        margin-bottom: 5px; /* Ensure there is space below textarea */
     }
 
     .button-container {
         display: flex;
         justify-content: center; /* Centers the button horizontally */
-        width: 100%; /* Full width of the container */
-        padding-bottom: 1em;
+        width: 100%; /* Ensures full width */
+        margin-top: 10px; /* Adds space above the button */
     }
 
     .action-button {
         padding: 10px 20px;
-        cursor: pointer; /* Makes it clear it's clickable */
-        margin-top: 10px; /* Space above the button */
+        cursor: pointer; /* Indicates it's clickable */
+    }
+
+    .controls {
+        display: flex;
+        justify-content: space-between; /* Keeps space between elements */
+        align-items: center;
+        width: 100%; /* Ensures full-width container */
+    }
+
+    #nview-select {
+        /* padding: 10px; /* Padding inside the dropdown */
+        /* display: block; /* Ensures that the select takes up only necessary space */
+        /* margin-left: auto; /* Pushes the dropdown to the right */
+    }
+
+    .nview-control {
+        display: flex;
+        /* align-items: center; */
     }
 </style>
